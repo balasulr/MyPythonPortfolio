d9a5a64d6650f41b4ba491325fb0fd5c4479030d
balasulr <lrb697@gmail.com>
2021-01-09 18:39:20 -0500

    - Added some insights from graphs made in TitanicGraphData.py - Imported TitanicFilterGraphData.py and calling all functions from TitanicFilterGraphData.py - Updated comment at top

--- Titanic.Data.Visualization.py
+++ Titanic.Data.Visualization.py
@@ -1,20 +1,25 @@
-# The code for the functions listed below are located in the
-# TitanicInitialFxn and TitanicSelectingAndSlicing files.
+# The code for the functions listed below are located in the TitanicInitialFxn,
+# TitanicSelectingAndSlicing, TitanicFilterGraphData, and TitanicGraphData.
+
+"""
+Insights from Titanic Data Set:
+1) Out of 891 people in dataset, 64.76% are male & 35.24% female.
+2) In addition, 61.62% died and 35.24% survived.
+3) The ages range from 0 to 80. There is only a difference of 6
+years between the oldest first and third class people.
+"""
 
 try:
-    # Bringing in files here & both files contain pandas
-    # which is used to perform the data visualization
+    # Importing files to be used that have functions inside:
     from TitanicInitialFxnTest import *
     from TitanicSelectingAndSlicing import *
-    dashlinebreak()  # Function created in
-    # TitanicInitialFxnTest to print line break
 
-    # Below functions come from TitanicInitialFxnTest
+    # Below function came from TitanicInitialFxnTest
     # Functions to get to know the data better
-    firstlast()
-    dashlinebreak()
+    first_last_five_rows()
+    dashlinebreak()  # Function created in TitanicInitialFxnTest to print line break
 
-    checkingcoldatatypes()
+    checking_col_data_types()
     dashlinebreak()
 
     # Below functions come from TitanicSelectingAndSlicing
@@ -26,6 +31,22 @@ try:
     dashlinebreak()
 
     pandaseriesgenderslice()
+    dashlinebreak()
+
+    # Importing the TitanicFilterGraphData
+    from TitanicFilterGraphData import *
+    # Below functions come from TitanicFilterGraphData
+    # to filter and graph data
+    pie_chart_gender()
+    dashlinebreak()
+
+    pie_chart_survived()
+    dashlinebreak()
+
+    class_fare_age_scatter()
+    dashlinebreak()
+
+    scatter_age_survive()
     dashlinebreak()
 
 except (RuntimeError, NameError, SyntaxError):
