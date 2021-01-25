80b6577da5b774996fe88aa308ababf8c5cb7e2c
balasulr <lrb697@gmail.com>
2021-01-25 13:24:28 -0500

    - Added 5 graphs from TitanicGraphData.py with the analysis in the function

--- TitanicFilterGraphData.py
+++ TitanicFilterGraphData.py
@@ -27,7 +27,7 @@ def pie_chart_gender():
 
     print("Pie chart showing males and females")
 
-    gender_graph()  # Pie chart of number of males and females
+    gender_graph()  # Graph
 
 
 def pie_chart_survived():
@@ -40,7 +40,7 @@ def pie_chart_survived():
 
     print("Pie chart showing Dead and Survived")
 
-    survived_graph()
+    survived_graph()  # Graph
 
 
 def class_fare_age_scatter():
@@ -53,7 +53,7 @@ def class_fare_age_scatter():
     print("Much of the data is clustered together for the second and third class.")
 
     print("\nScatter plot of Fare Payed and Age based on gender")
-    fare_age_by_gender()
+    fare_age_by_gender()  # Graph
 
 
 def scatter_age_survive():
@@ -64,3 +64,56 @@ def scatter_age_survive():
     print('and the survived column in 1 has more ones.')
 
     scatter_age_survive_sex()  # Graph
+
+
+def hist_embarked():
+    print('Histogram of Embarked passengers')
+
+    # Analysis of graph
+    print('\nThe Titanic went from Southampton(S) to Cherbourg (C) to Queenstown (Q).')
+    print('\nMore people embarked from Southampton then dropped more than one third in Cherbourg.')
+
+    embarked_hist()  # Graph
+
+
+def scatter_embark_age_class():
+    print('Passenger Class based on what port entered in on and age')
+
+    # Analysis of graph
+    print("\nThe oldest first and third class passenger embarked on in Port S.")
+
+    print('The age gap increased between passengers with each port.')
+
+    embark_age_class()  # Graph
+
+
+def scatter_age_fare_sex():
+    print('Showing the gender of various passenger based on the Fare and Age')
+
+    # Analysis of graph
+    print('\nMost people below 66 paid below 50 British Pounds.')
+
+    print('\nThe fare for both genders remains constant till age 30, when')
+    print('for the males, the fare makes a wide V, while the females get a narrow V.')
+
+    age_fare_sex_scatter()  # Graph
+
+
+def scatter_age_fare_survive():
+    print('Showing the survival of passengers based on the Fare and Age')
+
+    # Analysis of graph
+    print('\nThere is a general upward trend when looking at the fare and age for ')
+    print('those who survived in different directions when starting at the left.')
+    print('\nThe deaths form a upwards U shape around 35 to 40.')
+
+    age_fare_survive_scatter()  # Graph
+
+
+def scatter_age_fare_embark_port():
+    print('Showing the embarked port for passengers based on the Fare and Age')
+
+    # Analysis of graph
+    print('\nThe fare in the passengers who embarked in Cherbourg had the biggest gap.')
+
+    age_fare_embark_port_scatter()  # Graph
